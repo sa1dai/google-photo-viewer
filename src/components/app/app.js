4d4e5c47ac7a1487b5ff8d5e6841b2e0d8081186
Maxim Boychenko <maxim.boichenko@gmail.com>
2019-06-18 20:00:20 +0300

    удаление ненужных файлов после create-react-app

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './app.css';
+import Header from "src/components/header";
+
+const App = () => {
+  return (
+    <main role="main" className="main">
+      <Header />
+      <h1>Hello World</h1>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/header/header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <span>Header</span>
+  );
+};
+
+export default Header;
