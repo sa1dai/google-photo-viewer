5f4eea3a5e292faec5d74f8a3c80ec3a85b58b27
Maxim Boychenko <maxim.boichenko@gmail.com>
2019-06-20 08:28:41 +0300

    auth

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,30 +1,63 @@
 import React, { Component } from 'react';
-import { withRouter } from "react-router-dom";
+import config from 'src/auth/config';
+
 import './app.css';
 
 class App extends Component {
   componentDidMount() {
-    this._passport = require('passport');
-    const auth = require('./../../auth/auth');
-    auth(this._passport);
-    this._passport.initialize();
-    this._passport.session();
+    const _onInit = auth2 => {
+      console.log('init OK', auth2)
+    };
+
+    const _onError = err => {
+      console.log('error', err)
+    };
+
+    window.gapi.load('auth2', function() {
+      window.gapi.auth2
+        .init({
+          client_id: config.oAuthClientID,
+        })
+        .then(_onInit, _onError)
+    });
   }
 
-  onLogin = () => {
-    this.props.history.push('/auth/google');
+  signIn = () => {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+
+    auth2.signIn().then(googleUser => {
+      const profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // не посылайте подобную информацию напрямую, на ваш сервер!
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+
+      // токен
+      const id_token = googleUser.getAuthResponse().id_token;
+      console.log('ID Token: ' + id_token);
+    })
+  };
+
+  signOut = () => {
+    const auth2 = window.gapi.auth2.getAuthInstance();
+
+    auth2.signOut().then(function() {
+      console.log('User signed out.')
+    });
   };
 
   render() {
     return (
-      <button
-        className="btn btn-primary"
-        onClick={this.onLogin}
-      >
-        Login google photos
-      </button>
-    );
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.signIn}>Log in</button>
+          <button onClick={this.signOut}>Log out</button>
+        </header>
+      </div>
+    )
   }
 }
 
-export default withRouter(App);
+export default App;
