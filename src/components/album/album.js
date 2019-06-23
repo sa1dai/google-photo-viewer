26e5f7d4f3e0f4b8271c2f72ee5f5f4bb137e4a3
Maxim Boychenko <maxim.boichenko@gmail.com>
2019-06-23 19:25:49 +0300

    img gallery

--- src/components/album/album.js
+++ src/components/album/album.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import './album.css';
+import ImgsViewer from "react-images-viewer";
 
 const AlbumImg = ({ imgUrl }) => {
 
@@ -15,11 +16,59 @@ const AlbumImg = ({ imgUrl }) => {
 
 class Album extends Component {
 
-  onAlbumClick = () => {
+  componentWillMount() {
+    this.setState({
+      isOpen: false,
+      currImg: 0,
+    });
+  }
 
+  openImgsViewer = (index, event) => {
+    event.preventDefault();
+
+    this.setState({
+      currImg: index,
+      isOpen: true,
+    });
+  };
+
+  closeImgsViewer = () => {
+    this.setState({
+      currImg: 0,
+      isOpen: false,
+    });
+  };
+
+  gotoPrev = () => {
+    this.setState({
+      currImg: this.state.currImg - 1
+    });
+  };
+
+  gotoNext = () => {
+    this.setState({
+      currImg: this.state.currImg + 1
+    });
+  };
+
+  gotoImg = (index) => {
+    this.setState({
+      currImg: index
+    });
+  };
+
+  handleClickImg = () => {
+    if (this.state.currImg === this.props.imgs.length - 1) return;
+    this.gotoNext();
+  };
+
+  onAlbumClick = (event) => {
+    this.openImgsViewer(0, event);
   };
 
   render() {
+    console.log('render');
+
     const { album } = this.props;
 
     return (
@@ -27,6 +76,23 @@ class Album extends Component {
         <AlbumImg imgUrl={album.coverPhotoBaseUrl} />
         <div>{album.title}</div>
         <div>{album.mediaItemsCount}</div>
+        <ImgsViewer
+          backdropCloseable
+          currImg={this.state.currImg}
+          imgs={this.props.imgs}
+          isOpen={this.state.isOpen}
+          onClickImg={this.handleClickImg}
+          onClickNext={this.gotoNext}
+          onClickPrev={this.gotoPrev}
+          onClickThumbnail={this.gotoImg}
+          onClose={this.closeImgsViewer}
+          preventScroll={this.props.preventScroll}
+          showThumbnails={this.props.showThumbnails}
+          spinner={this.props.spinner}
+          spinnerColor={this.props.spinnerColor}
+          spinnerSize={this.props.spinnerSize}
+          theme={this.props.theme}
+        />
       </div>
     )
   }
